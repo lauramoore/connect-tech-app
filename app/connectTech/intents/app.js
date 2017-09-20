765c8b5c48e31e179f12f505a854dcacf1259546
Laura <laura.moore@rightbox.com>
2017-09-20 15:09:34 +0000

    ron swanson

--- app/connectTech/intents/app.js
+++ app/connectTech/intents/app.js
@@ -35,7 +35,14 @@ let connectTech = function (app) {
      });
 
      app.intent('getRonSwansonQuote', (request, response) => {
-
+       return app.ronSwansonApi.getQuote().then( (quote) => {
+             let finalQuote = quote;
+             app.makeCard(finalQuote, response, 'ron');
+             return response.say(`Ron Swanson Says: ${finalQuote}.
+                                 Would you like to hear another quote?`)
+                                 .shouldEndSession(false, 'Say that again?')
+                                 .send();
+         });
      });
 
      app.intent('audioPlayer', {
